89f8ee5ce937ed3ee3d309f7c88f2d6893159743
Bruno Leonardo de Sousa Cunha <brunob734@gmail.com>
2026-05-04 21:25:22 -0300

    Add files via upload

---
+++ script1.1.js
@@ -0,0 +1,9 @@
+class Livro{
+    constructor(titulo,autor){
+this.tituloLivro = titulo
+this.autorLivro = autor
+    }
+
+}
+const livroId01 = new Livro("Madeira Molhada","Jailson")
+console.log("O nome do livro é"+livroId01.tituloLivro + "O nome do autor do livro é: " + livroId01.autorLivro )
---
+++ script2.1.js
@@ -0,0 +1,10 @@
+class Pessoa{
+    constructor(nome){
+this.meuNome = nome
+    }
+    falar(){
+        console.log("Olá meu nome é:" + personagem.meuNome)
+    }
+}
+const personagem = new Pessoa ("Bruno")
+personagem.falar()
